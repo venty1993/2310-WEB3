e4e3f7b55a2f9c9e9fbea71e302c383a20575e0b
venty1993 <75829033+venty1993@users.noreply.github.com>
2023-10-27 11:12:58 +0900

    Update 3.페이지.js

--- 캐러셀/3.페이지.js
+++ 캐러셀/3.페이지.js
@@ -1,5 +1,38 @@
-// 나우페이지 가져오기
-// 토탈페이지 가져오기
+const contentsWrapper = document.querySelector('.contents-wrapper');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const 슬라이드갯수 = contentsWrapper.childElementCount
+const nowPage = document.querySelector('.now-page')
+const totalPage = document.querySelector('.total-page');
 
-// 인덱스에 따라 나우페이지가 변하게끔
-// 토탈페이지는 
+let index = 0;
+
+totalPage.innerText = 슬라이드갯수;
+console.log(슬라이드갯수)
+
+prevButton.addEventListener('click', ()=>{
+    
+    index--;
+    if(index < 0){
+        index = 슬라이드갯수-1;
+    }
+
+    위치적용();
+})
+nextButton.addEventListener('click',()=>{
+    index++;
+    if(index > 슬라이드갯수-1) {
+        index = 0;
+    }
+    위치적용();
+})
+
+function 위치적용() {
+    contentsWrapper.style.transform = 
+    `translateX(${index * -300}px)`
+
+    nowPage.innerText = index+1;
+}
+
+
+
