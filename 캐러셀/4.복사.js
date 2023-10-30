148ddf59916d613e79c16818df0d4a523a296a65
venty1993 <75829033+venty1993@users.noreply.github.com>
2023-10-30 12:54:09 +0900

    1

--- 캐러셀/4.복사.js
+++ 캐러셀/4.복사.js
@@ -5,24 +5,59 @@ const 슬라이드갯수 = contentsWrapper.childElementCount
 const nowPage = document.querySelector('.now-page')
 const totalPage = document.querySelector('.total-page');
 
-let index = 0;
+const 한번에보는슬라이드=1;
+
+
+
+//복사붙여넣기 하고나면 슬라이드의 갯수가 늘어나기때문에
+// line 4의 갯수를 파악하는 구문 실행 이후에 노드를 복제해야한다.
+const 첫번째페이지복사본 = contentsWrapper.firstElementChild.cloneNode(true);
+const 마지막페이지복사본 = contentsWrapper.lastElementChild.cloneNode(true);
+
+contentsWrapper.appendChild(첫번째페이지복사본);
+contentsWrapper.insertBefore(마지막페이지복사본,contentsWrapper.firstElementChild);
+
+
+
+
+
+
+let index = 1;
 
 totalPage.innerText = 슬라이드갯수;
 console.log(슬라이드갯수)
 
 prevButton.addEventListener('click', ()=>{
     
-    index--;
-    if(index < 0){
-        index = 슬라이드갯수-1;
-    }
+    // index--;
+    // if(index < 1){
+    //     index = 슬라이드갯수;
+    // }
 
+    // 위치적용();
+
+    index--;
     위치적용();
+
+    if(index === 한번에보는슬라이드 - 1) {
+        // 애니메이션 끝난다음에 = 1초뒤에
+        setTimeout(() => {
+        //    setTimeout 함수에는 두개의 인수를 전달한다.
+        // 실행할 함수(콜백), 몇초 뒤에 실행할것인지 시간(ms)
+
+
+        // 인덱스를 마지막페이지로 넘기고
+        index = 슬라이드갯수 + 한번에보는슬라이드 - 1;
+        // 적용, 이때는 트랜지션 X
+
+
+        }, timeout);
+    }
 })
 nextButton.addEventListener('click',()=>{
     index++;
-    if(index > 슬라이드갯수-1) {
-        index = 0;
+    if(index > 슬라이드갯수) {
+        index = 1;
     }
     위치적용();
 })
@@ -32,11 +67,11 @@ function 위치적용() {
     contentsWrapper.style.transform = 
     `translateX(${index * -300}px)`
 
-    nowPage.innerText = index+1;
+    nowPage.innerText = index - 한번에보는슬라이드 + 1;
 
     contentsWrapper.style.transition = `1s`
 
-    버튼색변경(index);
+    버튼색변경(index-한번에보는슬라이드);
 }
 
 
@@ -65,4 +100,3 @@ for(let i = 0 ; i < 하단버튼.length ; i ++) {
         위치적용();
     })
 }
-
